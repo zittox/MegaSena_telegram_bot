8d72572942bc80bf59f9e6c2e8e92d2c2a621ca6
zito <zitotreverson@gmail.com>
2022-03-25 11:25:15 -0300

    v4 + pesquisa concurso a fazer - start/stop e range errado do input do concurso

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-# Bot Telegram pra geração de jogos da MegaSena
+# Bot de Telegram pra geração de jogos da MegaSena
 
-## Opções de jogos: 6, 7, 8 ou 9 números + resultado do último concurso
-
-
+## Opções de jogos: 6, 7, 8 ou 9 números 
+##+ resultado do último concurso 
+##+ pesquisa por número de concurso
+>_____________________________
+>>Usando api de resultados do [Gustavo Alves](https://github.com/guto-alves/loterias-api)
+> _____________________________
    #   ![_small_Screenshot_20220315-220450_Telegram](https://user-images.githubusercontent.com/67715164/158497243-196a3b23-945a-4e61-8d62-d0597d21815e.jpg)
 
 ### No momento o bot @MegaSenaBRbot está:
@@ -16,8 +19,11 @@
 - Clone o repositorio localmente
 - Crive e ative sua virtualenv
 - Instale o reqeuirements.txt
-- Crie um arquivo chave.txt, no mesmo diretório raiz, e cole nele sua chave feita no @BotFather do TELEGRAM
-
->>Usando api de resultados do [Gustavo Alves](https://github.com/guto-alves/loterias-api)
-
+- Instale o dotenv
+> pip install python-dotenv
+- Crie um arquivo .env, no mesmo diretório raiz, e cole nele sua chave feita no @BotFather do TELEGRAM
+> chave = 'blablbalablabalabalbaalabalbbla'
 ### -->>>>> Contribuições são bem-vindas _/\_
+
+
+
--- telbot.py
+++ telbot.py
@@ -1,93 +1,156 @@
 import telebot
 from telebot import types
-import time
+from telebot.types import KeyboardButton, ReplyKeyboardMarkup
 import qrandom
 import requests
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+token = os.getenv('chave')
+
+b = telebot.TeleBot(token)
 
 # api da caixa https://github.com/guto-alves/loterias-api
 apicaixa = 'https://loteriascaixa-api.herokuapp.com/api/mega-sena/latest'
 
+
 def ultimo_resultado():
+    apicaixa = 'https://loteriascaixa-api.herokuapp.com/api/mega-sena/latest'
     r = requests.get(apicaixa).json()
-    data = f" concurso: {r['concurso']}\ndata: {r['data']}\ndezenas: {r['dezenas']}\nacumulou: {r['acumulou']}\nprox_premio: {r['acumuladaProxConcurso']}\nprox_concurso: {r['dataProxConcurso']}"
-    return data
+    data = f"Concurso: {r['concurso']}"
+    data1 = f"Data: {r['data']}"
+    data2 = "Dezenas: {} - {} - {} - {} - {} - {}".format(r['dezenas'][0], r['dezenas'][1], r['dezenas'][2],
+                                                          r['dezenas'][3], r['dezenas'][4], r['dezenas'][5])
+    if r['premiacoes'][0]['vencedores'] == 0:
+        venc = f"Vencedores: Não"
+    else:
+        venc = f"Vencedores: {r['premiacoes'][0]['vencedores']}"
+    if r['premiacoes'][0]['premio'] == "-":
+        prem = f"Premiações: 0"
+    else:
+        prem = f"Premiações: R$ {r['premiacoes'][0]['premio']}"
+    if r['acumulou'] == True:
+        data3 = "Acumulou: Sim"
+    else:
+        data3 = "Acumulou: Não"
+    data4 = f"Prox_prêmio: {r['acumuladaProxConcurso']}"
+    data5 = f"Prox_concurso: {r['dataProxConcurso']}"
+    return f'{data}\n{data1}\n{data2}\n{venc}\n{prem}\n{data3}\n{data4}\n{data5}'
 
 
-with open("chave.txt", "r") as c:
-    chave = c.read()
-
-b = telebot.TeleBot(chave)
-
+bini = types.KeyboardButton('/start')
+bpara = types.KeyboardButton('/stop')
+b6 = KeyboardButton('6')
+b7 = KeyboardButton('7')
+b8 = KeyboardButton('8')
+b9 = KeyboardButton('9')
+bur = KeyboardButton('último resultado')
+bp = KeyboardButton('pesquise concurso')
+ba = KeyboardButton('ajuda')
+binfo = KeyboardButton('Info')
+k1 = ReplyKeyboardMarkup(resize_keyboard=True)
+k1.row(b6, b7, b8, b9)
+k1.row(bur, bp)
+k1.row(ba,binfo)
+k1.row(bini,bpara)
 
 
 @b.message_handler(commands=['start'])
 def resp1(menss):
-    markup = types.ReplyKeyboardMarkup(row_width=1)
-    n6 = types.KeyboardButton('/6_números')
-    n7 = types.KeyboardButton('/7_números')
-    n8 = types.KeyboardButton('/8_números')
-    n9 = types.KeyboardButton('/9_números')
-    nur = types.KeyboardButton('/último_resultado')
-    naj = types.KeyboardButton('/ajuda')
-    markup.add(n6,n7,n8,n9,nur,naj)
-    b.reply_to(menss, "\n Bem vindo ao gerador de jogo(s) da Mega Sena\n Este bot somente funciona com os botões na própria tela\nQuantos números tem o jogo da MEGA SENA que você quer fazer?", reply_markup=markup)
+    b.reply_to(menss,
+               """Bem vindo ao gerador de jogo(s) da Mega Sena!!!  ->> O único momento que vc não vai usar os botões da tela, é quando pesquisar por concurso!\nPara fazer um jogo de 6, 7 , 8 ou 9 números: clique nos núnemros\nOu escolha uma das outras opções""",
+               reply_markup=k1)
 
-@b.message_handler(commands=["último_resultado"])
-def nur(menss):
-    b.reply_to(menss, ultimo_resultado())
+# será q quando tiver hosteado na nuvem ele volta funcionar com /strat?
+@b.message_handler(commands=['stop'])
+def resp2(menss):
+    b.reply_to(menss, 'Valeus, Até a próxima')
+    b.stop_bot()
 
-@b.message_handler(commands=["6_números"])
-def n6(menss):
-    j = qrandom.sample(range(1, 61), 6)
-    j.sort()
-    jogox2 = ""
-    while j:
-        for i in j:
+
+@b.message_handler()
+def qual_conc(menss):
+    if menss.text == 'Info':
+        b.reply_to(menss, '''MegaSenaBRbot v4\n
+        ~~~~ >>> feito por github.com/zittox/\n
+        ~~~ >>> api pesquisa de resultado por github.com/guto-alves/loterias-api\n
+        Boa sorte na jogatina\n  :":": └[∵┌] └[ ∵ ]┘ [┐∵]┘ :":": \n ''')
+    if menss.text == '6':
+        j = qrandom.sample(range(1, 61), 6)
+        j.sort()
+        jogox2 = ""
+        while j:
             jogox2 = " - ".join(str(i) for i in j)
-        break
-    b.reply_to(menss, f'Seu jogo é:  {jogox2}')
-
-@b.message_handler(commands=["7_números"])
-def n7(menss):
-    j = qrandom.sample(range(1, 61), 7)
-    j.sort()
-    jogox2 = ""
-    while j:
-        for i in j:
+            break
+        b.reply_to(menss, f'Seu jogo é:  {jogox2}')
+    elif menss.text == '7':
+        j = qrandom.sample(range(1, 61), 7)
+        j.sort()
+        jogox2 = ""
+        while j:
             jogox2 = " - ".join(str(i) for i in j)
-        break
-    b.reply_to(menss, f'Seu jogo é:  {jogox2}')
-
-@b.message_handler(commands=["8_números"])
-def n8(menss):
-    j = qrandom.sample(range(1, 61), 8)
-    j.sort()
-    jogox2 = ""
-    while j:
-        for i in j:
+            break
+        b.reply_to(menss, f'Seu jogo é:  {jogox2}')
+    elif menss.text == '8':
+        j = qrandom.sample(range(1, 61), 8)
+        j.sort()
+        jogox2 = ""
+        while j:
             jogox2 = " - ".join(str(i) for i in j)
-        break
-    b.reply_to(menss, f'Seu jogo é:  {jogox2}')
-
-@b.message_handler(commands=["9_números"])
-def n9(menss):
-    j = qrandom.sample(range(1, 61), 9)
-    j.sort()
-    jogox2 = ""
-    while j:
-        for i in j:
+            break
+        b.reply_to(menss, f'Seu jogo é:  {jogox2}')
+    elif menss.text == '9':
+        j = qrandom.sample(range(1, 61), 9)
+        j.sort()
+        jogox2 = ""
+        while j:
             jogox2 = " - ".join(str(i) for i in j)
-        break
-    b.reply_to(menss, f'Seu jogo é:  {jogox2}')
+            break
+        b.reply_to(menss, f'Seu jogo é:  {jogox2}')
+    elif menss.text == 'ajuda':
+        b.send_message(menss.chat.id, 'Para fazer um novo jogo ou pesquisar resultados, clique nos botões abaixo')
+    elif menss.text == 'último resultado':
+        b.reply_to(menss, ultimo_resultado())
+    elif menss.text == 'pesquise concurso':
+        sentm = b.send_message(menss.chat.id, "Digite o número do concurso")
+        b.register_next_step_handler(sentm, concurso)
 
-@b.message_handler(content_types=['ajuda','text'])
-def texto(menss):
-    b.send_message(menss.chat.id, 'Para fazer um novo jogo clique nos botões')
+
+def concurso(menss): # fazer a negativa de quando digitar um numero fora da range
+    conc = menss.text
+    apicaixaconc = f'https://loteriascaixa-api.herokuapp.com/api/mega-sena/{conc}'
+    r = requests.get(apicaixaconc).json()
+    data = f"Concurso: {r['concurso']}"
+    data1 = f"Data: {r['data']}"
+    data2 = "Dezenas: {} - {} - {} - {} - {} - {}".format(r['dezenas'][0], r['dezenas'][1], r['dezenas'][2],
+                                                          r['dezenas'][3], r['dezenas'][4], r['dezenas'][5])
+    if r['premiacoes'][0]['vencedores'] == 0:
+        venc = f"Vencedores: Não"
+    else:
+        venc = f"Vencedores: {r['premiacoes'][0]['vencedores']}"
+    if r['premiacoes'][0]['premio'] == "-":
+        prem = f"Premiações: 0"
+    else:
+        prem = f"Premiações: R$ {r['premiacoes'][0]['premio']}"
+    if r['acumulou'] == True:
+        data3 = "Acumulou: Sim"
+    else:
+        data3 = "Acumulou: Não"
+    data4 = f"Prox_prêmio: {r['acumuladaProxConcurso']}"
+    data5 = f"Prox_concurso: {r['dataProxConcurso']}"
+    b.reply_to(menss, f'{data}\n{data1}\n{data2}\n{venc}\n{prem}\n{data3}\n{data4}\n{data5}')
+
 
 @b.message_handler(content_types=['photo'])
 def foto(menss):
     b.send_message(menss.chat.id, 'bela foto')
+
+
+@b.message_handler(content_types=['input'])
+def qqtexto(menss):
+    b.reply_to(menss, 'Para fazer um novo jogo ou pesquisar resultados, clique nos botões abaixo')
+
 
 
 
